3581bf660161c3a8cc06eeff88cb8d1aa3bf1e27
thechucklingatom <thechucklingatom@153.90.56.205>
2015-01-28 13:24:17 -0700

    Added functions, have a test file to read and get working before probably switching for buffered input for final, implemented a string split for new lines to have smaller strings to work through and to be able to control the parse a little better.

--- HuffmanCode/src/huffmancode/FrequencyTable.java
+++ HuffmanCode/src/huffmancode/FrequencyTable.java
@@ -10,9 +10,37 @@ package huffmancode;
  * @author thechucklingatom
  */
 public class FrequencyTable {
+    private int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, 
+        v, x, y, z, sp, nl, aps, quote;
+    //for ease of iteration god i'm rusty
+    private int[] arr  = { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, 
+        r, s, t, u, v, x, y, z, sp, nl, aps, quote};
     
-    public void CreateTable(){
-
+    //handles a just one line
+    public void CreateTable(String line){
+        
+    }
+    
+    //handles a multiline document(split at returns)
+    public void CreateTable(String[] doc){
+        nl = doc.length;
+    }
+    
+    //resets the table for a different input
+    public void ResetTable(){
+        //set everything to 0
+    }
+    
+    private void ParseString(String line){
+        //Frequency of each line.
+    }
+    
+    private void ParseString(String[] doc){
+        //will go through and find the frequency of each char
+    }
+    
+    private void AddToQ(){
+        //add the arr into a Q to be returned as a frequency table
     }
 }
 
---
+++ HuffmanCode/src/huffmancode/HuffmanCode.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huffmancode;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author thechucklingatom
+ */
+public class HuffmanCode {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        File file = new File("test.txt");
+        byte[] data;
+        try (FileInputStream fis = new FileInputStream(file)) {
+            data = new byte[(int) file.length()];
+            fis.read(data);
+        }
+
+        String str = new String(data, "UTF-8");
+        
+        String lines[] = str.split("\\r?\\n");
+        
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+    
+}
